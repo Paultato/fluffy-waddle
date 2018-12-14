63976eb42cdadf8ed8f9d72e2d0d557c0f2f7bea
Paul Banus <paul.banus@epita.fr>
2018-12-14 19:39:43 +0100

    La haine

--- src/Game/PlayerIA/PaultatoPlayer.php
+++ src/Game/PlayerIA/PaultatoPlayer.php
@@ -41,15 +41,27 @@ class PaultatoPlayer extends Player
         // How can i display the result of each round ? $this->prettyDisplay()
         // -------------------------------------    -----------------------------------------------------
         
+        // Coucou Robin j'espère ta correction se passe bien et que tu me mettras 20 MERCI
+        
         $nbRound = $this->result->getNbRound();
+        // La famille TMTC
         $gangDuT9 = array('Santost', 'Mattiashell', 'Vcollette');
+        // On apprend le nom de l'adversaire et si c'est pas la famille je le brise
         $oppName = $this->result->getStatsFor($this->opponentSide)['name'];
+        // Le score de l'adversaire pour savoir si il est mieux que moi et le tacler après il va voir flou
+        $oppScore = $this->result->getStatsFor($this->opponentSide)['score'];
+        // Mon score de bon codeur qui documente beaucoup le code
+        $myScore = $this->result->getStatsFor($this->mySide)['score'];
 
+        // On régale la famille TMTC
         if ($nbRound === 9) {
             if (in_array($oppName, $gangDuT9))
                 return parent::friendChoice();
             return parent::foeChoice();
         }
+
+        if ($myScore < $oppScore)
+            return parent::foeChoice();
 
         if ($nbRound != 0) {
             $foePercent = $this->result->getStatsFor($this->opponentSide)['foe'] / $nbRound;
