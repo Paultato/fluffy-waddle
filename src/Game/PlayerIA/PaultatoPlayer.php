f76e473e3f75f55cb02d363c5100cfa091d874b7
Paul Banus <paul.banus@epita.fr>
2018-12-14 19:45:38 +0100

    La haine documentée

--- src/Game/PlayerIA/PaultatoPlayer.php
+++ src/Game/PlayerIA/PaultatoPlayer.php
@@ -41,11 +41,16 @@ class PaultatoPlayer extends Player
         // How can i display the result of each round ? $this->prettyDisplay()
         // -------------------------------------    -----------------------------------------------------
         
+
+
+
         // Coucou Robin j'espère ta correction se passe bien et que tu me mettras 20 MERCI
         
         $nbRound = $this->result->getNbRound();
         // La famille TMTC
         $gangDuT9 = array('Santost', 'Mattiashell', 'Vcollette');
+        // Les ennemis de la nation
+        $ennemis = array('Akatsuki95', 'Vegan60');
         // On apprend le nom de l'adversaire et si c'est pas la famille je le brise
         $oppName = $this->result->getStatsFor($this->opponentSide)['name'];
         // Le score de l'adversaire pour savoir si il est mieux que moi et le tacler après il va voir flou
@@ -60,14 +65,22 @@ class PaultatoPlayer extends Player
             return parent::foeChoice();
         }
 
+        // Parce que je suis un mec haineux et que j'aime la gratuité de la violence
         if ($myScore < $oppScore)
             return parent::foeChoice();
 
+        // Parce que je déteste certaines personnes de la classe
+        if (in_array($oppName, $ennemis))
+                return parent::foeChoice();
+
+        // Détection de ratio de haine de l'adversaire grâce à un réseau de neurone
         if ($nbRound != 0) {
             $foePercent = $this->result->getStatsFor($this->opponentSide)['foe'] / $nbRound;
             if ($foePercent > 0.5)
                 return parent::foeChoice();
         }
+
+        // C'est pour la forme, je prétends être gentil des fois
         return parent::friendChoice();
     }
  
