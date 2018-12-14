34f64d9e3363b8c611d0b9891641f07f4bdb528c
Paul Banus <paul.banus@epita.fr>
2018-12-14 19:56:52 +0100

    Author

--- src/Game/PlayerIA/PaultatoPlayer.php
+++ src/Game/PlayerIA/PaultatoPlayer.php
@@ -7,50 +7,25 @@ use Hackathon\Game\Result;
 /**
  * Class LovePlayer
  * @package Hackathon\PlayerIA
- * @author PaulB
+ * @author Paul Banus
  */
 class PaultatoPlayer extends Player
 {
     protected $mySide;
     protected $opponentSide;
     protected $result;
+    
+    
+    // La famille TMTC 
+    private $gangDuT9 = array('Santost', 'Mattiashell', 'Vcollette', 'Neosia67'); 
+    // Les ennemis de la nation
+    private $ennemis = array('Akatsuki95', 'Vegan60');
 
     public function getChoice()
     {
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
-        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the stats                ?    $this->result->getStats()
-        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the number of round      ?    $this->result->getNbRound()
-        // -------------------------------------    -----------------------------------------------------
-        // How can i display the result of each round ? $this->prettyDisplay()
-        // -------------------------------------    -----------------------------------------------------
-        
+        // Coucou Robin j'espère ta correction se passe bien et que tu me mettras 20 MERCI   
 
-
-
-        // Coucou Robin j'espère ta correction se passe bien et que tu me mettras 20 MERCI
-        
         $nbRound = $this->result->getNbRound();
-        // La famille TMTC
-        $gangDuT9 = array('Santost', 'Mattiashell', 'Vcollette', 'Neosia67');
-        // Les ennemis de la nation
-        $ennemis = array('Akatsuki95', 'Vegan60');
         // On apprend le nom de l'adversaire et si c'est pas la famille je le brise
         $oppName = $this->result->getStatsFor($this->opponentSide)['name'];
         // Le score de l'adversaire pour savoir si il est mieux que moi et le tacler après il va voir flou
